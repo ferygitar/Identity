00e0430af9cdfb9c2b0a3ce792a5d6dba5bfe94d
Faramarz Laely nasab <faraweb.plus@gmail.com>
2021-08-24 13:59:49 +0430

    1400-06-02

---
+++ Identity/ViewModels/ManageUser/AddUserToRoleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.ViewModels.ManageUser
+{
+    public class AddUserToRoleViewModel
+    {
+        public AddUserToRoleViewModel()
+        {
+            UserRoles = new List<UserRolesViewModel>();
+        }
+
+        public string UserId { get; set; }
+
+        public List<UserRolesViewModel> UserRoles { get; set; }
+    }
+
+   public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
